c95435afef720040cb705ea0e940046d940cdcd9
freemp3droid <freemp3droid@e0af2044-716c-cc46-887c-2b746bdfafe8>
2010-09-03 21:44:05 +0000

    move init of stopFile into library

--- src/org/freemp3droid/Main.java
+++ src/org/freemp3droid/Main.java
@@ -1,5 +1,8 @@
 package org.freemp3droid;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
@@ -9,19 +12,19 @@ import android.os.Bundle;
 import android.os.IBinder;
 
 public class Main extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    	if(getIntent() !=null && getIntent().getAction().equals("org.freemp3droid.CONVERT")){
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if(getIntent() !=null && getIntent().getAction().equals("org.freemp3droid.CONVERT")){
 			Intent convIntent = new Intent(Main.this, MP3Service.class);
 			startService(convIntent);
 			bindService(convIntent, mConvertConnection, Context.BIND_AUTO_CREATE);
-    	} else {
-    		finish();
-    	}
-    }
-    
-    private ServiceConnection mConvertConnection = new ServiceConnection() {
+		} else {
+			finish();
+		}
+	}
+
+	private ServiceConnection mConvertConnection = new ServiceConnection() {
 		@Override
 		public void onServiceConnected(ComponentName name, IBinder service) {
 			MP3Service mp3Service = ((MP3Service.LocalBinder)service).getService();
@@ -29,6 +32,15 @@ public class Main extends Activity {
 			String mp3File = getIntent().getExtras().getString("mp3File");
 			String progressFile = getIntent().getExtras().getString("progressFile");
 			String stopFile = getIntent().getExtras().getString("stopFile");
+			//need to touch the file. 1 means stop, anything else means go.
+			try {
+				FileOutputStream ofo = new FileOutputStream("stop.txt");
+				ofo.write("0".getBytes());
+				ofo.flush();
+				ofo.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
 			int sampleRate =  getIntent().getExtras().getInt("sampleRate");
 			int bitRate =  getIntent().getExtras().getInt("bitRate");
 			mp3Service.convertMP3File(convertFile, mp3File, progressFile, stopFile, sampleRate, bitRate);
@@ -37,7 +49,7 @@ public class Main extends Activity {
 		@Override
 		public void onServiceDisconnected(ComponentName name) {	}	
 	};
-	
+
 	@Override
 	protected void onPause() {
 		super.onPause();
